2c36885b231b31c37ba857d5e10ff22e7cab2f31
Miloslav Číž <drummyfish@disroot.org>
2020-09-11 20:24:41 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,3 +1,35 @@
+# Build script for Anarch.
+# by drummyfish, released under CC0 1.0, public domain
+
 #!/bin/bash
 
-clear; clear; g++ -x c -g -Wall -Wextra -fmax-errors=5 -pedantic -O3 -Wall -Wextra -Wno-unused-parameter -Wno-missing-field-initializers -o game main.c -lSDL2 2>&1 >/dev/null && ./game
+if [ $# -ne 1 ]; then
+  echo "need a parameter (sdl, pokitto, gb, ...)"
+  exit 0
+fi
+
+clear; clear; 
+
+if [ $1 == "sdl" ]; then
+
+  # PC SDL build, requires:
+  # - SDL2 (dev) package
+
+  g++ -x c -g -Wall -Wextra -fmax-errors=5 -pedantic -O3 -Wall -Wextra \
+  -Wno-unused-parameter -Wno-missing-field-initializers -o game main_sdl.c \
+  -lSDL2 2>&1 >/dev/null && ./game
+elif [ $1 == "pokitto" ]; then
+
+  # Pokitto build, requires:
+  # - PokittoLib, in this folder create a symlink named "PokittoLib" to the 
+  #   "Pokitto" subfolder of PokittoLib
+  # - Pokitto Makefile
+  # - GNU embedded toolchain, in this folder create a symlink named "gtc" to the
+  #   "bin" subfolder
+  # - files like My_settings.h required by Pokitto
+
+  make
+  ./PokittoEmu BUILD/firmware.bin 
+else
+  echo "unknown parameter: $1"
+fi
