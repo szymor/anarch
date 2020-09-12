4a67999c2807976d5849812c6d20d729ffe1c8c3
Miloslav Číž <drummyfish@disroot.org>
2020-09-12 11:34:11 +0200

    Balance weapons and monsters

--- main_sdl.c
+++ main_sdl.c
@@ -26,9 +26,8 @@
 #define SFG_LOG(str) puts(str);
 
   #define SFG_START_LEVEL 6
-  #define SFG_UNLOCK_DOOR 1
   #define SFG_IMMORTAL 1
-  #define SFG_UNLOCK_DOOR 1
+//  #define SFG_UNLOCK_DOOR 1
   #define SFG_REVEAL_MAP 1
   #define SFG_INFINITE_AMMO 1
 
