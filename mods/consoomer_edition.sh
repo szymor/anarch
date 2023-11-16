71fd9953fb82fdf5df4ee681beb7fc52175f95e9
Miloslav Ciz <drummyfish@disroot.org>
2023-11-16 19:37:22 +0100

    Update consoomer edition

--- mods/consoomer_edition.sh
+++ mods/consoomer_edition.sh
@@ -1,20 +1,18 @@
 #/bin/sh
 # Applies mods to Anarch to create a consoomer edition more suitable for a
-# mainstream gaymer. Run from the root directory of the repository.
+# mainstream gaymer. Run from the root directory of the repository (NOT the
+# mod directory).
 # by drummyfish
 
 echo "making consoomer edition"
 
-git apply mods/particle_effects.diff
-git apply mods/hd/hd.diff
-git apply mods/crt.diff
-git apply mods/movement_inertia.diff
-git apply mods/full_zbuffer.diff
-git apply mods/demo.diff
-git apply mods/terminal_messages.diff
-git apply mods/weapon_swing.diff
+for f in "particle_effects" "quicksave" "terminal_messages" "hd/hd" "crt" "movement_inertia" "full_zbuffer" "weapon_swing"
+do
+  echo "=== APPLYING $f ==="
+  git apply --ignore-space-change --ignore-whitespace mods/$f.diff 2>&1 >/dev/null
+done
 
 sed -i 's/\(SFG_VERSION_STRING ".*\)"/\1 consoomer"/g' texts.h
-sed -i 's/SFG_PLAYER_DAMAGE_MULTIPLIER 512/SFG_PLAYER_DAMAGE_MULTIPLIER 300/g' settings.h # make the game easier
+sed -i 's/SFG_PLAYER_DAMAGE_MULTIPLIER 512/SFG_PLAYER_DAMAGE_MULTIPLIER 300/g' settings.h # make the game easier for normies
 
 echo "DONE"
